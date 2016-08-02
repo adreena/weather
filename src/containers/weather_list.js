13f333cb6f46ab1b8577685c04f6eeda22435653
Kimia Hassanzadeh <kim.hszd@gmail.com>
2016-08-01 23:58:04 -0600

    added sparklines

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component{
+  renderWeather(cityData){
+
+      const temps = cityData.list.map(weather=>weather.main.temp);
+      const humidities = cityData.list.map(weather=>weather.main.humidity);
+      const pressures = cityData.list.map(weather=>weather.main.pressure);
+
+      return (
+          <tr key={cityData.city.name}>
+            <td>{cityData.city.name}</td>
+            <td><Chart data={temps} color='red' units='K'/></td>
+            <td><Chart data={pressures} color='green' units='hPa'/></td>
+            <td><Chart data={humidities} color='orange' units='%'/></td>
+          </tr>
+      );
+  }
+
   render(){
     return(
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hpa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
